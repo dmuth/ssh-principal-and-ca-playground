c4b0736cf77865827df0d27c7ff85e20fe68bfe8
Douglas Muth <dmuth@dmuth.org>
2019-12-22 14:20:09 -0500

    Added .profile to tell what user we logged in as.

--- files/entrypoint-server.sh
+++ files/entrypoint-server.sh
@@ -23,6 +23,11 @@ cp -v /root/.ssh/authorized_keys /home/user1/.ssh/
 cp -v /root/.ssh/authorized_keys /home/user2/.ssh/
 cp -v /root/.ssh/authorized_keys /home/user3/.ssh/
 
+cp -v /profile /root/.profile
+cp -v /profile /home/user1/.profile
+cp -v /profile /home/user2/.profile
+cp -v /profile /home/user3/.profile
+
 chown -R user1 /home/user1/
 chown -R user2 /home/user2/
 chown -R user3 /home/user3/
